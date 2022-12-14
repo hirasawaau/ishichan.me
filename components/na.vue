1fe5584bcf99dc2e62549ce0e26fde8a41b1dc11
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-12-14 17:25:46 +0700

    Initial Project

---
+++ components/na.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-sheet min-height="70vh" class="d-flex justify-center align-center">
+    <p class="text-h2">This page is not avaliable</p>
+  </v-sheet>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row align="center">
+    <v-col cols="12">
+      <p class="text-h2">Hirasawa Ishigawa</p>
+    </v-col>
+    <v-col cols="6">
+      <v-card>
+        <v-card-title class="text-xs-h6 text-md-h5 text-lg-h4"
+          >Title</v-card-title
+        >
+        <v-card-text> Body text </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="6">
+      <v-card>
+        <v-card-title class="text-xs-h6 text-md-h5 text-lg-h4"
+          >Title</v-card-title
+        >
+        <v-card-text> Body text </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<style>
+p .text-h2 {
+  margin: 30em;
+}
+</style>
+
+<script lang="ts"></script>
