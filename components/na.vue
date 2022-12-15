6cc2ca33a21649d658e067d3bbdeac4c273e25c1
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2022-12-15 20:38:41 +0700

    chore: Migrate to Remix

--- components/na.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-sheet min-height="70vh" class="d-flex justify-center align-center">
-    <p class="text-h2">This page is not avaliable</p>
-  </v-sheet>
-</template>
--- pages/certificates.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <Na />
-</template>
